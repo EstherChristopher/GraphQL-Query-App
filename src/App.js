8e3a960e7b4985bd28ba5da1e186dfbb2a452368
Adamu Esther Christopher <estheradamuchris@gmail.com>
2023-03-29 20:59:45 +0100

    graphql-tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery, gql } from "@apollo/client";
+
+const GET_CHARACTERS = gql`
+  query {
+    characters {
+      results {
+        name
+        gender
+        image
+      }
+    }
+  }
+`;
+
+function DisplayCharacters() {
+  const { loading, error, data } = useQuery(GET_CHARACTERS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return data.characters.results.map(({ name, gender, image }) => (
+    <div key={name}>
+      <h3>{name}</h3>
+      <img width="400" height="300" alt="character" src={`${image}`} />
+      <br />
+      <b>Gender:</b>
+      <p>{gender}</p>
+      <br />
+    </div>
+  ));
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rick and Morty Characters</h1>
+      <DisplayCharacters />
     </div>
   );
 }
-
 export default App;
